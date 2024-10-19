f9ebb23280fbb5a8a8f0e5b47ebb44a63973eba2
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-10-19 15:30:45 +0800

    Updates

---
+++ assets/index-Co9tj9xO.js
@@ -0,0 +1,1 @@
+import{j as t,M as s,O as e}from"./index-DujYdSVm.js";const r=()=>t.jsx(s,{title:"Sales",children:t.jsx(e,{})});export{r as default};
--- assets/index-DfCx6ItP.js
+++
@@ -1,1 +0,0 @@
-import{j as t,M as e,O as a}from"./index-J2ZmRp0j.js";const s=()=>t.jsx(e,{title:"Account Management",children:t.jsx(a,{})});export{s as default};
