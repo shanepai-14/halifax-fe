13f7c76c642422d2a5647f20a55cb4259e7b2bc6
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-10-20 13:56:38 +0800

    Updates

---
+++ assets/index-65hjrMWA.js
@@ -0,0 +1,1 @@
+import{j as t,M as e,O as r}from"./index-e8asW0S_.js";const n=()=>t.jsx(e,{title:"Inventory",children:t.jsx(r,{})});export{n as default};
--- assets/index-BH-tOGRm.js
+++
@@ -1,1 +0,0 @@
-import{j as t,M as s,O as e}from"./index-BhYoBfNI.js";const r=()=>t.jsx(s,{title:"Sales",children:t.jsx(e,{})});export{r as default};
