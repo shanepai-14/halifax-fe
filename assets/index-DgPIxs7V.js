fdc77ca00ec3988d96fe268e55ed964d96431243
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-10-20 21:37:01 +0800

    Updates

---
+++ assets/index-DgPIxs7V.js
@@ -0,0 +1,1 @@
+import{j as t,M as e,O as r}from"./index-AD2Xt7AV.js";const a=()=>t.jsx(e,{title:"Supplier",children:t.jsx(r,{})});export{a as default};
---
+++ assets/index-DspcnOqJ.js
@@ -0,0 +1,1 @@
+import{j as t,M as r,O as s}from"./index-AD2Xt7AV.js";const a=()=>t.jsx(r,{title:"Product",children:t.jsx(s,{})});export{a as default};
